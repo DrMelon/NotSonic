0050849fc89dfc870e93def30ba6e56e32dc0ca8
J Brown <drmelon.mail@gmail.com>
2015-06-02 00:26:01 +0100

    Animations expanded

--- NotSonic/Entities/SonicPlayer.cs
+++ NotSonic/Entities/SonicPlayer.cs
@@ -22,10 +22,12 @@ namespace NotSonic.Entities
 
 
             // Create sprites
-            spriteSheet = new Spritemap<string>(Assets.SONIC_SHEET, 32, 40);
+            spriteSheet = new Spritemap<string>(Assets.SONIC_SHEET, 40, 40);
             spriteSheet.Add("idle", new int[] { 0 }, new float[] { 6f });
-            spriteSheet.Add("roll", new int[] { 1 }, new float[] { 6f });
-            spriteSheet.Add("spindash", new int[] { 2 }, new float[] { 6f });
+            spriteSheet.Add("walk", new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }, new float[] { 6f });
+            spriteSheet.Add("run", new int[] { 9, 10, 11, 12}, new float[] { 6f });
+            spriteSheet.Add("roll", new int[] { 13, 14, 13, 15, 13, 16, 13, 17 }, new float[] { 12f });
+            spriteSheet.Add("spindash", new int[] { 18 }, new float[] { 6f });
             spriteSheet.Play("idle");
             Graphic = spriteSheet;
             this.Layer = 18;
@@ -59,7 +61,7 @@ namespace NotSonic.Entities
             // Rotations
             if (myMovement.CurrentFloorMode == NotSonic.Components.SonicMovement.FloorMode.FLOOR)
             {
-                Graphic.Angle = 0;          
+                Graphic.Angle = 0;   
             }
             if(myMovement.CurrentFloorMode == NotSonic.Components.SonicMovement.FloorMode.RIGHTWALL)
             {
@@ -73,19 +75,49 @@ namespace NotSonic.Entities
             {
                 Graphic.Angle = 270;
             }
+
+
             
 
             if(myMovement.Rolling)
             {
                 spriteSheet.Play("roll");
+                for (int i = 0; i < 8; i++)
+                {
+                    spriteSheet.Anims["roll"].FrameDelays[i] = Math.Max(5 - Math.Abs(myMovement.GroundSpeed), 1);
+
+                }
+                    
                 Graphic.Angle = 0;
                 //Graphic.OriginY = 15;
 
             }
-            else
+            else if (Math.Abs(myMovement.GroundSpeed) < 0.01)
             {
                 spriteSheet.Play("idle");
                 
+            }
+            else if (Math.Abs(myMovement.GroundSpeed) < myMovement.TopSpeed)
+            {
+
+                //walking 
+                spriteSheet.Play("walk");
+                for (int i = 0; i < 8; i++)
+                {
+                    spriteSheet.Anims["walk"].FrameDelays[i] = Math.Max(8 - Math.Abs(myMovement.GroundSpeed), 1);
+
+                }
+            
+            }
+            else
+            {
+                // Running
+                spriteSheet.Play("run");
+                for (int i = 0; i < 4; i++)
+                {
+                    spriteSheet.Anims["run"].FrameDelays[i] = Math.Max(8 - Math.Abs(myMovement.GroundSpeed), 1);
+
+                }
             }
 
             if(myMovement.CurrentSpindashStrength > 0.0f)
